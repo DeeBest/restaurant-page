26f0ec38002fbe02b745512a1073fd2db0e6bb5c
DeeBest <simphiwedladla8@gmail.com>
2024-03-10 12:35:28 +0200

    downloaded images that will be used in the home page images cards.

--- src/Pages/home-page/homePage.js
+++ src/Pages/home-page/homePage.js
@@ -6,9 +6,25 @@ import './homePage.css';
 export function createHomePage(){
   const homePageContent = document.createElement('div');
   homePageContent.classList.add('home-page-content');
-  homePageContent.textContent = 'Welcome To My Restaurant Page';
-
-
   
+  const imagesDiv = document.createElement('div');
+  imagesDiv.classList.add('images-div');
+  imagesDiv.textContent = 'images-div'
+  homePageContent.appendChild(imagesDiv);
+
+  const imageCard1 = document.createElement('div');
+  imageCard1.classList.add('image-card1');
+  imagesDiv.appendChild(imageCard1);
+
+  const imageCard2 = document.createElement('div'); 
+  imageCard2.classList.add('images-card2');
+  imagesDiv.appendChild(imageCard2);
+
+  const imageCard3 = document.createElement('div');
+  imageCard3.classList.add('images-card3');
+  imagesDiv.appendChild(imageCard3);
+
+
+
   mainContentContainer.appendChild(homePageContent)
 };
