694cb617d9c821657d731e31884c57cb0fcbdee0
DeeBest <simphiwedladla8@gmail.com>
2024-03-11 18:14:15 +0200

    i have created a contact form and applied styling to it for the contact page

--- src/Pages/contact-page/contact.js
+++ src/Pages/contact-page/contact.js
@@ -6,7 +6,51 @@ export function createContactPage(){
   mainContentContainer.textContent = '';
 
   const contactPageContent = document.createElement('div');
-  contactPageContent.textContent = 'contact';
+  contactPageContent.id = 'contact-page-content';
+
+  const contactPageHeader = document.createElement('h1');
+  contactPageHeader.textContent = 'Give Us Your Feedback';
+
+  const contactForm = document.createElement('form');
+  contactForm.id = 'contact-form';
+
+  const nameLabel = document.createElement('label');
+  nameLabel.textContent = 'Name:';
+
+  const nameInput = document.createElement('input');
+  nameInput.type = 'text';
+  nameInput.name = 'name';
+
+  contactForm.appendChild(nameLabel);
+  contactForm.appendChild(nameInput);
+
+  const emailLabel = document.createElement('label');
+  emailLabel.textContent = 'Email:';
+
+  const emailInput = document.createElement('input');
+  emailInput.type = 'email';
+  emailInput.name = 'email';
+
+  contactForm.appendChild(emailLabel);
+  contactForm.appendChild(emailInput);
+
+  const messageLabel = document.createElement('label');
+  messageLabel.textContent = 'Message:';
+
+  const messageInput = document.createElement('textarea');
+  messageInput.name = 'message';
+
+  contactForm.appendChild(messageLabel);
+  contactForm.appendChild(messageInput);
+
+  const submitButton = document.createElement('button');
+  submitButton.type = 'submit';
+  submitButton.textContent = 'Submit';
+  contactForm.appendChild(submitButton);
+
+
+  contactPageContent.appendChild(contactPageHeader);
+  contactPageContent.appendChild(contactForm);
 
   mainContentContainer.appendChild(contactPageContent);
 };
